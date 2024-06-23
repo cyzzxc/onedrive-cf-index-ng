be9e44895616421cf0fc1e7d008c14c3c1b2fb38
cyzzxc <51287400+cyzzxc@users.noreply.github.com>
2024-06-23 13:40:44 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -30,5 +30,5 @@ module.exports = {
   // - s-maxage=0: cache is fresh for 60 seconds on the edge, after which it becomes stale
   // - stale-while-revalidate: allow serving stale content while revalidating on the edge
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
-  cacheControlHeader: 'max-age=600, s-maxage=60, stale-while-revalidate',
+  cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
 }
